14a619679aa812aa20a544a68ffe20c060196018
BHKim <byeongho.kim@mytrum.com>
2022-08-23 18:36:45 +0900

    Functional Component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square" onClick={()=>this.props.onClick()}>
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={()=>props.onClick}>
+            {props.value}
+        </button>
+    )
 }
 
 class Board extends React.Component {
@@ -24,8 +24,13 @@ class Board extends React.Component {
         return <Square
             value={this.state.squares[i]}
             onClick={()=>this.handleClick(i)}
-
         />;
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
     }
 
     render() {
